b6ca915b7c26299df52ef59fbeec573404af6f89
Nikos Anastopoulos <n.anastop@gmail.com>
2018-02-14 16:25:52 +0200

    Remove sleep

--- memory_tests/seqacc.c
+++ memory_tests/seqacc.c
@@ -23,7 +23,7 @@ static void sigalrm_hnd(int sig)
     timer_stop(&t);
     fprintf(stderr, "elements_processed:%ld, cycles_per_element:%lf, ", 
                     i, timer_total(&t)/(double)i);
-    sleep(1);
+    //sleep(1);
     i = 0;
     timer_clear(&t);
     timer_start(&t);
